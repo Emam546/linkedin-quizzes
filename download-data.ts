3999255cb88dab30d5758b7efe63e5add8cbf7ca
Emam546 <emam54637@gmail.com>
2023-06-27 13:50:13 +0200

    update teh reading method

--- download-data.ts
+++ download-data.ts
@@ -5,15 +5,15 @@ import AdmZip from "adm-zip";
 import path from "path";
 
 const downloadAndExtractZip = async (url: string) => {
+    const filepath = path.join(__dirname, "temp.zip");
     try {
         // Download the zip file
         const response = await axios.get(url, { responseType: "arraybuffer" });
 
         // Save the downloaded zip file
-        fs.writeFileSync(path.join(__dirname, "temp.zip"), response.data);
-
+        response.data.pipe(fs.createWriteStream(filepath));
         // Extract the zip file
-        const zip = new AdmZip("temp.zip");
+        const zip = new AdmZip(filepath);
         zip.extractAllTo(folderPath, true);
 
         console.log("Zip file downloaded and extracted successfully.");
@@ -21,7 +21,7 @@ const downloadAndExtractZip = async (url: string) => {
         console.error("Error:", error.message);
     } finally {
         // Clean up - delete the temporary zip file
-        fs.unlinkSync("temp.zip");
+        fs.unlinkSync(filepath);
     }
 };
 const owner = "Ebazhanov";
